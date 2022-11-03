48e7097b856fb065752d49728e55e87a760ca52c
stephaniemaf <farrell.s.ava@gmail.com>
2022-11-03 14:20:06 +0000

    changed heading font size for small screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,121 +1,121 @@
-const compPlay = document.getElementById("computer-choice")
-const userPlay = document.getElementById("user-choice")
-const resultDisp= document.getElementById("result")
-const choices = document.querySelectorAll("button")
-var scorePlayer = document.getElementById("playerScore")
-var scoreComputer = document.getElementById("computerScore")
-var scoreDraw = document.getElementById("drawScore")
-let playerChoice
-let computerChoice
-let result
+const compPlay = document.getElementById("computer-choice");
+const userPlay = document.getElementById("user-choice");
+const resultDisp= document.getElementById("result");
+const choices = document.querySelectorAll("button");
+var scorePlayer = document.getElementById("playerScore");
+var scoreComputer = document.getElementById("computerScore");
+var scoreDraw = document.getElementById("drawScore");
+let playerChoice;
+let computerChoice;
+let result;
 
 choices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    playerChoice = e.target.id
-    userPlay.innerHTML = playerChoice
-    compChoice()
-    playGame()
-  }))
+    playerChoice = e.target.id;
+    userPlay.innerHTML = playerChoice;
+    compChoice();
+    playGame();
+  }));
 
 function compChoice(){
-    const randNum = Math.floor(Math.random()* 5) + 1
+    const randNum = Math.floor(Math.random()* 5) + 1;
     switch(randNum){
         case 1:
-            computerChoice = "rock"
+            computerChoice = "rock";
             break;
         case 2:
-            computerChoice = "paper"
+            computerChoice = "paper";
             break;
         case 3: 
-            computerChoice = "scissors"
+            computerChoice = "scissors";
             break;
         case 4:
-            computerChoice = "lizard"
+            computerChoice = "lizard";
             break;
         case 5:
-            computerChoice = "spock"
+            computerChoice = "spock";
             break;
         default:
-            computerChoice = "invalid"
+            computerChoice = "invalid";
     }
-    compPlay.innerHTML = computerChoice
+    compPlay.innerHTML = computerChoice;
   }
 
 function playGame(){
     if(computerChoice === playerChoice){
-        result = "You chose the same!! Draw"
-        scoreDraw.innerHTML = parseInt(scoreDraw.innerHTML)+1
+        result = "You chose the same!! Draw";
+        scoreDraw.innerHTML = parseInt(scoreDraw.innerHTML)+1;
     }   else{
         switch(playerChoice){
             case "rock":
                 if(computerChoice === "scissors" )
-                {result = "WooHoo!!! You Won"
-                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1}
+                {result = "WooHoo!!! You Won";
+                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1;}
                 else if(computerChoice === "lizard")
-                {result = "WooHoo!!! You Won"
-                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1}
+                {result = "WooHoo!!! You Won";
+                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1;}
                 else if(computerChoice === "paper")
-                {result = "Aww! You lost" 
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}
+                {result = "Aww! You lost"; 
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}
                 if(computerChoice === "spock")
-                {result = "Aww! You lost" 
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}
+                {result = "Aww! You lost";
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}
                 break;
             case "paper":
                 if(computerChoice === "rock")
-                {result = "WooHoo!!! You Won"
-                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1}
+                {result = "WooHoo!!! You Won";
+                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1;}
                 else if(computerChoice === "scissors" )
-                {result = "Aww! You lost"
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}
+                {result = "Aww! You lost";
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}
                 else if(computerChoice === "lizard")
-                {result = "Aww! You lost"
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}
+                {result = "Aww! You lost";
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}
                 else if(computerChoice === "spock")
-                {result = "WooHoo!!! You Won"
-                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1}
+                {result = "WooHoo!!! You Won";
+                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1;}
                 break;
             case "scissors":
                 if(computerChoice === "rock")
-                {result = "Awww You lost!"
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}
+                {result = "Awww You lost!";
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}
                 else if(computerChoice === "paper")
-                {result = "WooHoo!!! You Won"
-                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1}
+                {result = "WooHoo!!! You Won";
+                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1;}
                 else if(computerChoice === "lizard")
-                {result = "WooHoo!!! You Won"
-                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1}
+                {result = "WooHoo!!! You Won";
+                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1;}
                 else if(computerChoice === "spock")
-                {result = "Awww You lost!"
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}
+                {result = "Awww You lost!";
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}
                 break;
             case "lizard":
                 if(computerChoice === "rock")
-                {result = "Awww You lost!"
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}
+                {result = "Awww You lost!";
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}
                 else if(computerChoice === "paper" )
-                {result = "WooHoo!!! You Won"
-                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1}
+                {result = "WooHoo!!! You Won";
+                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1;}
                 else if(computerChoice === "scissors")
-                {result = "Aww! You lost"
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}
+                {result = "Aww! You lost";
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}
                 else if(computerChoice === "spock")
-                {result = "Aww! You lost"
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}
+                {result = "Aww! You lost";
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}
                 break;
             case "spock":
                 if(computerChoice === "rock")
-                {result = "WooHoo!!! You Won"
-                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1}
+                {result = "WooHoo!!! You Won";
+                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1;}
                 else if(computerChoice === "paper")
-                {result = "Awww You lost!"
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}
+                {result = "Awww You lost!";
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}
                 else if(computerChoice === "scissors")
-                {result = "WooHoo!!! You Won"
-                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1}
+                {result = "WooHoo!!! You Won";
+                scorePlayer.innerHTML = parseInt(scorePlayer.innerHTML)+1;}
                 else if(computerChoice === "lizard")
-                {result = "Aww! You lost"
-                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1}             
+                {result = "Aww! You lost";
+                scoreComputer.innerHTML = parseInt(scoreComputer.innerHTML)+1;}             
         }    
     } 
-    resultDisp.innerHTML = result
+    resultDisp.innerHTML = result;
 }
